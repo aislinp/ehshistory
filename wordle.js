a38f00d9cdfeed0ea6d0c120d414c95fe149f664
Aislin :) <10750181+aislinp@users.noreply.github.com>
2024-03-06 09:16:19 -0800

    remove histordle from js

--- wordle.js
+++ wordle.js
@@ -1,21 +1,8 @@
-var histordle = document.getElementById('histordle');
 var worldle = document.getElementById('worldle');
 var yeardle = document.getElementById('yeardle');
 
-var histordleButton = document.getElementById('histbutton');
 var worldleButton = document.getElementById('worldbutton');
 var yeardleButton = document.getElementById('yearbutton');
-
-histordleButton.addEventListener('click', hist);
-function hist(){
-  	if(histordle.style.display == 'block'){
-		histordle.style.display = 'none';
-	} else {
-		histordle.style.display = 'block';
-		worldle.style.display = 'none';
-		yeardle.style.display = 'none';
-	}
-}
 
 worldleButton.addEventListener('click', world);
 function world(){
